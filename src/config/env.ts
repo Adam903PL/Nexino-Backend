5293e56dacddab0cd1e7eeed3ebd012e2e011455
Adam Pukaluk <pukaluk.adam505@gmail.com>
2025-04-09 10:40:45 +0200

    condig/env.ts checking if rnv exists

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,14 +2,20 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const ENV = {
-  PORT: process.env.PORT ?? 3333,
-  JWT_SECRET:process.env.JWT_SECRET ?? "",
-  COIN_GEKO_API:process.env.COIN_GEKO_API ??"",
-  REDIS_URL:process.env.REDIS_URL ??"",
-  REDIS:{
-    HOST:process.env.REDIS_HOST ?? '127.0.0.1',
-    PORT:process.env.REDIS_PORT ??  6379
+
+export const getEnvironment = (name:string) => {
+  const  environment = process.env[name]
+  if(!environment){
+    throw new Error(`❌ Missing ${name} environment`)
   }
-  
+  return environment
+}
+
+
+
+export const ENV = {
+  PORT: getEnvironment("PORT"),
+  JWT_SECRET:getEnvironment("JWT_SECRET"),
+  COIN_GEKO_API:getEnvironment("COIN_GEKO_API"),
+  REDIS_URL:getEnvironment("REDIS_URL"),
 };
