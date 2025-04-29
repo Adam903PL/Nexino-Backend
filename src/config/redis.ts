1600e47ee6a903e2709d9b7df51df3d1cc90489b
Adam Pukaluk <pukaluk.adam505@gmail.com>
2025-04-29 12:46:27 +0200

    init

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,7 +1,28 @@
 import Redis from "ioredis";
 import { ENV } from "./env";
 
-export const redis = new Redis(ENV.REDIS_URL);
+export const redis = new Redis({
+
+  host: new URL(ENV.REDIS_URL).hostname, 
+  port: 6379, 
+  password: new URL(ENV.REDIS_URL).password, 
+  tls: {},
+  connectTimeout: 20000, 
+});
+
+
 redis.on("error", (err) => {
   console.error("Redis Error:", err);
+});
+
+redis.on("connect", () => {
+  console.log("Połączono z Redisem");
+});
+
+redis.on("ready", () => {
+  console.log("Redis gotowy do użycia");
+});
+
+redis.on("close", () => {
+  console.log("Połączenie z Redisem zostało zamknięte");
 });
